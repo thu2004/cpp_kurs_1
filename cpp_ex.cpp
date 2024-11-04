64e0376215ec4690147f88631be38bf15a545193
Chi Thu Le <thu2004@yahoo.se>
2024-11-04 12:07:53 +0100

    Add files via upload

---
+++ cpp_ex.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int car_age;
+    cout << "Hur gammal är din bil? ";
+    cin >> car_age;
+
+    if (car_age <= 5) {
+        cout << "Du borde skaffa helförsäkring" << endl;
+    }
+    else if (car_age > 5 && car_age < 25) {
+        cout << "Du borde skaffa halvförsäkring" << endl;
+    }
+    else if (car_age >= 25) {
+        cout << "Du borde skaffa veteransbilsförsäkring" << endl;
+    }
+}
